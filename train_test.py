1597aa35c49868c11d4a196987b46668f395316e
Sophie Hao <yiding.hao@yale.edu>
2023-02-13 12:59:40 -0500

    Added train_test module

---
+++ train_test.py
@@ -0,0 +1,81 @@
+"""
+Code for Problem 4 of HW 2.
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from datasets import Dataset
+from tqdm import tqdm
+
+from model import LSTMSentimentClassifier
+
+
+def evaluate(model: LSTMSentimentClassifier, test_data: Dataset,
+             batch_size: int = 32) -> float:
+    """
+    Problem 4b: Complete the implementation of this function. You may
+    write code anywhere in the function body. You must remove the raise
+    statement on line 38.
+
+    Evaluate an LSTM sentiment classifier on a testing dataset. In order
+    to save memory, the evaluation should occur in mini-batches, such
+    that the model is only being evaluated on a few examples at a time.
+
+    :param model: The model being evaluated
+    :param test_data: The data to evaluate the model with
+    :param batch_size: The size of the mini-batches
+
+    :return: The accuracy of the model on test_data, defined as the
+        total proportion of examples (between 0 and 1) that have been
+        classified correctly
+    """
+    model.eval()
+    with torch.no_grad():
+        for i in tqdm(range(0, len(test_data), batch_size)):
+            batch = test_data[i:i + batch_size]
+            raise NotImplementedError("Problem 4b has not been completed yet!")
+
+
+def train(model: LSTMSentimentClassifier, train_data: Dataset,
+          val_data: Dataset, batch_size: int = 32, max_epochs: int = 5,
+          patience: int = 2, lr: float = .01, filename: str = "model.pt"):
+    """
+    Problem 4c: Complete the implementation of this function. You may
+    write code anywhere in the function body, though comments have been
+    provided to guide the placement of your code. You must remove the
+    raise statements on lines 74 and 82.
+
+    :param model: The model to be trained
+    :param train_data: The data to train the model on
+    :param val_data: The data to use for early stopping
+    :param batch_size: The size of the mini-batches for training and
+        evaluation on validation data
+    :param max_epochs: The maximum number of times ("epochs") to loop
+        through all the training data
+    :param patience: If this many consecutive epochs have passed without
+        observing the best validation accuracy so far, then end training
+        even if the maximum number of epochs has not yet been reached
+    :param lr: The learning rate to use for Adam
+    :param filename: The name of the file to save the model to
+    """
+    loss_function = nn.CrossEntropyLoss()
+    adam = optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(max_epochs):
+        print("Epoch {} of {}".format(epoch + 1, max_epochs))
+
+        # Training code
+        print("Training...")
+        model.train()
+        for i in tqdm(range(0, len(train_data), batch_size)):
+            batch = train_data[i:i + batch_size]
+            # TODO: Write your training code here
+            raise NotImplementedError("Problem 4c has not been completed yet!")
+
+        # Test on validation data
+        print("Evaluating on validation data...")
+        val_acc = evaluate(model, val_data, batch_size=batch_size)
+        print("Validation accuracy: {:.3f}".format(val_acc))
+
+        # TODO: Write your early stopping code here
+        raise NotImplementedError("Problem 4c has not been completed yet!")
